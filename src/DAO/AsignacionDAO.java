0e0f89be2d5cec0040c38ddb59c8ed7e655a3e1f
Francisco Dueñas <frsduenasca@unal.edu.co>
2019-06-14 10:07:16 -0500

    Se agrego el package "DAO" con las clases "AsignacionDAO" y "RestauranteDAO"

---
+++ src/DAO/AsignacionDAO.java
@@ -0,0 +1,17 @@
+package DAO;
+
+public class AsignacionDAO {
+
+    public void crear() {
+
+    }
+
+    public void ver() {
+
+    }
+
+    public void editar() {
+
+    }
+
+}
---
+++ src/DAO/RestauranteDAO.java
@@ -0,0 +1,16 @@
+package DAO;
+
+public class RestauranteDAO {
+
+    public void crear() {
+
+    }
+
+    public void ver() {
+
+    }
+
+    public void editar() {
+
+    }
+}
